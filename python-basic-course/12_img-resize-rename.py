c103bad724a30c99d9aaf8b3a72c5f18a01f1b37
fyk54 <hacgk@yahoo.com.hk>
2024-12-16 17:51:36 -0500

    Commit Done

--- python-basic-course/12_img-resize-rename.py
+++ python-basic-course/12_img-resize-rename.py
@@ -8,7 +8,7 @@ renamePath ='C:\\Users\\Eric Fung\\Desktop\\rename\\'
 #myTools.resizeImage(srcPath,destPath)
 #myTools.batchRename(renamePath)
 1
-mode = input("you want to (1 = rename, 2 = resize, 0 = leave)")
+mode = input("you want to (1 = rename, 2 = resize, 0 = leave) : ")
 
 if(mode=='1'):
     myTools.batchRename(renamePath)
